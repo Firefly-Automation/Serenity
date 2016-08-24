f6b1b3dadd3529efe6d7c87acc9b5493920bcb49
Zachary Priddy <me@zpriddy.com>
2016-08-23 22:23:54 -0700

    Inital Commit

---
+++ Serenity/core/view_generators.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# @Author: zpriddy
+# @Date:   2016-08-23 22:07:06
+# @Last Modified by:   zpriddy
+# @Last Modified time: 2016-08-23 22:20:07
+
+
+## build array of all templates minified
+## devices can have 'custom_view:true' and then 'custom_html:' as part of the device view
+## proccess the data that comes in and generate the device_vew json object
+## write the object to disk - Should only have to re-generate every few minutes
---
+++ Serenity/templates/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# @Author: zpriddy
+# @Date:   2016-08-19 21:24:26
+# @Last Modified by:   zpriddy
+# @Last Modified time: 2016-08-19 21:24:27
