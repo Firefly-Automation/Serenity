da92bbe557e0ea66934ce387c7110e2ef891a565
Zachary Priddy <me@zpriddy.com>
2016-10-13 01:15:52 -0700

    fixing token

--- Serenity/models.py
+++ Serenity/models.py
@@ -2,7 +2,7 @@
 # @Author: Zachary Priddy
 # @Date:   2016-08-29 12:46:04
 # @Last Modified by:   Zachary Priddy
-# @Last Modified time: 2016-10-13 01:11:11
+# @Last Modified time: 2016-10-13 01:15:41
 
 from Serenity import app
 
@@ -57,7 +57,7 @@ def auth_token_required(fn):
         rToken = request.get_json().get("token")
         argsToken = request.args.get('token')
         #return _get_unauthorized_response()
-        for token in AuthToken.objects:
+        for token in AuthToken.query.all():
             if rToken == token.token or argsToken == token.token:
                 print token.user_id
                 return fn(*args, **kwargs)
