bb58fd503533e9c47bc0fc569217b45f1781417d
Zachary Priddy <me@zpriddy.com>
2016-10-14 16:37:39 -0700

    fixing locative

--- Serenity/views.py
+++ Serenity/views.py
@@ -2,7 +2,7 @@
 # @Author: Zachary Priddy
 # @Date:   2016-08-29 12:04:59
 # @Last Modified by:   Zachary Priddy
-# @Last Modified time: 2016-10-14 16:32:44
+# @Last Modified time: 2016-10-14 16:37:24
 
 import json
 
@@ -144,10 +144,9 @@ def iftttAPI():
 @app.route('/API/locative', methods=['GET', 'POST'])
 @auth_token_required
 def locativeAPI():
-  url = API_PATHS['locative']
+  url = API_PATHS['locative'] + request.query_string
   print url
-  #return str(requests.post(url, data=request.query_string))
-  return str(request.args)
+  return str(requests.post(url))
 
 @app.route('/API/views/devices')
 @login_required
