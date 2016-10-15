695563c78aff27d6e652ee97297a30c4f96fe446
Zachary Priddy <me@zpriddy.com>
2016-10-14 18:01:26 -0700

    threaded

--- runserver.py
+++ runserver.py
@@ -2,12 +2,12 @@
 # @Author: Zachary Priddy
 # @Date:   2016-08-29 12:06:24
 # @Last Modified by:   Zachary Priddy
-# @Last Modified time: 2016-10-13 21:43:30
+# @Last Modified time: 2016-10-14 18:01:19
 
 from Serenity import *
 
 if S_debug:
-  app.run(host=S_host, port=S_port)
+  app.run(host=S_host, port=S_port, threaded=True)
 
 else:
   from gevent.wsgi import WSGIServer
